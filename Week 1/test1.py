3b5285cff25043736ffcd4cb936b5141893440d1
godprobe <godprobe.misc+gitcommit@gmail.com>
2019-10-18 01:05:38 -0700

    Week 3 and 4 exercise descriptions added, comment added to test1 to clarify Py2/Py3 compatability

--- Week 1/test1.py
+++ Week 1/test1.py
@@ -1,6 +1,6 @@
 from __future__ import print_function
 
-try:
+try: # Attempt raw_input for python2 compatability
 	ip_addr = raw_input("Enter an IP address: ")
 except NameError:
 	ip_addr = input("Enter an IP address: ")
