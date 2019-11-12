f247e42f153c3e516f6a9e754006706034a7c8a4
godprobe <godprobe.misc+git@gmail.com>
2019-11-12 14:10:26 -0800

    Week 2 exercise 3 done

---
+++ Week_2/c_Week2_Exercise3.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from __future__ import print_function, unicode_literals, with_statement
+from pprint import pprint
+
+# Read in the "show\_arp.txt" file using the `readlines()` method. Use a *list slice* to remove the header line.
+with open("show_arp.txt") as arpFile:
+    arps = arpFile.readlines()[1:]
+    # Use `pretty print` to print out the resulting list to the screen
+    pprint(arps)
+    # Create a new list slice that is only the first three ARP entries.
+    threeArps = arps[0:3]
+    # Use the `.join()` method to join these first three ARP entries back together as a single string
+    # using the newline character ('`\n`') as the separator.
+    # TODO: check if I really need '\n' instead of '' for this join; newlines already in the list items
+    arpString = ''.join(threeArps)
+    # Write this string containing the three ARP entries out to a file named "arp_entries.txt".
+    with open("arp_entries.txt",'w') as writeableFile:
+        writeableFile.write(arpString)
+
